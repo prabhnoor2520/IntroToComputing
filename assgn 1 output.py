f787c13f29b7f656ac3c8e3f92bf5e1983c481f4
prabhnoor2520 <97435664+prabhnoor2520@users.noreply.github.com>
2022-01-10 14:24:36 +0530

    Python Assignment 1

---
+++ assgn 1 output.py
@@ -0,0 +1,22 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\acer\AppData\Local\Programs\Python\Python310\assgn 1.py ==
+enter your first number:1
+enter your second number:2
+enter your third number:3
+2.0
+enter your gross income:40000
+enter total number of dependandts:2
+24000
+4800.0
+['Prabhnoor', '21104090', 'M', 'Python', '9.0']
+marks of student 1:45
+marks of student 2:55
+marks of student 3:65
+marks of student 4:75
+marks of student 5:85
+[45.0, 55.0, 65.0, 75.0, 85.0]
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
---
+++ assgn 1.py
@@ -0,0 +1,66 @@
+#question 1
+x=int(input("enter your first number:"))
+y=int(input("enter your second number:"))
+z=int(input("enter your third number:"))
+
+avg=(x+y+z)/3
+
+print(avg)
+
+
+
+#question 2
+GI=int(input("enter your gross income:"))
+SD=10000
+DD=3000
+Dep=int(input("enter total number of dependandts:"))
+
+TI=GI-SD-(DD*Dep)
+
+print(TI)
+
+Rate=0.2 #rate here is tax rate = 20%
+
+TAX=TI*Rate
+
+print(TAX)
+
+
+
+#question 3
+list=['Prabhnoor','21104090','M','Python','9.0']
+print(list)
+
+
+#question 4
+Num1=float(input('marks of student 1:'))
+Num2=float(input('marks of student 2:'))
+Num3=float(input('marks of student 3:'))
+Num4=float(input('marks of student 4:'))
+Num5=float(input('marks of student 5:'))
+
+Marks=[Num1, Num2, Num3, Num4, Num5]
+print(Marks)
+
+
+
+#question 5a
+color1=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print(color1)
+
+color1.pop(3)
+print(color1)
+
+#question 5b
+color2=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+color2[3:5]=['Purple']
+
+print(color2)
+
+
+
+      
+      
+
+
+

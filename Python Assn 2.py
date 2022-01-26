4bb0ac5d678b03993a42c7ac6252cfa8b6a6ffb4
prabhnoor2520 <97435664+prabhnoor2520@users.noreply.github.com>
2022-01-26 13:48:02 +0530

    Add files via upload

---
+++ Python Assn 2.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\acer\OneDrive\Desktop\python assgn 2.py ==========
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+enter your name:Prabhnoor
+enter your SID:21104090
+enter department name:Electrical
+enter your CGPA:10
+Hey, Prabhnoor Here! 
+My SID is 21104090 
+I am from Electrical and my CGPA is 10.000000
+8
+58
+50
+224
+40
+14
+2
+enter your first number:6
+enter your second number:7
+enter your third number:8
+Number_3 is the greatest
+enter a string:My name is Prabhnoor
+yes
+enter length of 1st side:6
+enter length of 2nd side:8
+enter length of 3rd side:10
+yes
